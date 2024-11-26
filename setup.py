d0856de4ae4a4ca78b3ceac5f0c976ff0162fc42
datadev13 <datadev13>
2024-11-26 20:58:10 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dbyoke",
-    version="0.1.0",
+    version="0.1.1",
     description="A library to unify database connections and drivers",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
